25fb30451eb628e92a38417f0e5eceba34459b03
tehica <tehica97@gmail.com>
2022-04-24 20:53:26 +0200

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : BaseApiController
+    {
+        private readonly StoreContext _db;
+
+        public ProductsController(StoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async  Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _db.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Product>>> GetProduct(int id)
+        {
+            var product = await _db.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
